5c40eda25843fb23e4c7059e6eca21faad5304dd
attiilla <atilappl@gmail.com>
2025-05-07 00:07:58 -0300

    sharing control on scripts

--- run.sh
+++ run.sh
@@ -15,6 +15,7 @@ fi
 
 prove=false
 log=false
+sharing=false
 
 # Process remaining arguments (1 or 2)
 for arg in "$@"; do
@@ -25,25 +26,36 @@ for arg in "$@"; do
         log)
             log=true
             ;;
+        sharing)
+            sharing=true
+            ;;
         *)
-            echo "Error: Invalid argument '$arg'. Only 'prove' and 'log' are allowed."
+            echo "Error: Invalid argument '$arg'. Only 'prove', 'sharing' and 'log' are allowed."
             exit 1
             ;;
     esac
 done
 
 if [ "$prove" = true ]; then
-    #./scripts/generate_proofs.sh clear "$folder"
-    echo "./scripts/generate_proofs.sh clear "$folder""
-    #./scripts/generate_proofs.sh "$folder"
-    echo "./scripts/generate_proofs.sh "$folder""
+    ./scripts/generate_proofs.sh clear "$folder"
+    #echo "./scripts/generate_proofs.sh clear "$folder""
+    ./scripts/generate_proofs.sh "$folder"
+    #echo "./scripts/generate_proofs.sh "$folder""
 fi
 
 ./scripts/filter_unknowns.sh "$folder"
 ./scripts/sanitize_result.sh "$folder"
 
 if [ "$log" = true ]; then
-    ./scripts/check_folder.sh "$folder" > run.log
+    if [ "$sharing" = true ]; then
+        ./scripts/check_folder.sh "$folder" sharing > run.log
+    else
+        ./scripts/check_folder.sh "$folder" > run.log
+    fi
 else
-    ./scripts/check_folder.sh "$folder"
+    if [ "$sharing" = true ]; then
+        ./scripts/check_folder.sh "$folder" sharing
+    else
+        ./scripts/check_folder.sh "$folder"
+    fi
 fi
--- scripts/check_folder.sh
+++ scripts/check_folder.sh
@@ -4,6 +4,7 @@
 process_file() {
     local alethe_file="$1"
     local smt2_file="$2"
+    local sharing="$3"
     local base_name="${alethe_file%.*}"  # Remove the extension to get the base name
     local original_lines compressed_lines diff diff_status
     
@@ -15,7 +16,12 @@ process_file() {
     fi
 
     # Run the second command 
-    ./target/debug/carcara compress --allow-int-real-subtyping --expand-let-bindings "$alethe_file" "$smt2_file" > "${base_name}.Calethe" 2>/dev/null
+    if [ "$sharing" = true ]; then
+        ./target/debug/carcara compress --allow-int-real-subtyping --expand-let-bindings "$alethe_file" "$smt2_file" > "${base_name}.Calethe" 2>/dev/null
+    else
+        ./target/debug/carcara compress --allow-int-real-subtyping --expand-let-bindings --no-print-with-sharing "$alethe_file" "$smt2_file" > "${base_name}.Calethe" 2>/dev/null
+    fi
+    
     if [ $? -ne 0 ]; then
         echo "Failed on compressing $alethe_file"
         return 2
@@ -31,18 +37,36 @@ process_file() {
     return 0
 }
 
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <folder_path> <args>"
+    exit 1
+fi
 
 dir=$1
+shift
 total=0
 worked=0
 check_err=0
 compress_err=0
 post_check_err=0
+sharing=false
 
 # Define counters for line difference comparisons
 positive_diff=0
 negative_diff=0
 zero_diff=0
+
+for arg in "$@"; do
+    case "$arg" in
+        sharing)
+            sharing=true
+            ;;
+        *)
+            echo "Error: Invalid argument '$arg'. Only 'sharing' is allowed."
+            exit 1
+            ;;
+    esac
+done
 
 # Find all .alethe files in the sample directory and its subdirectories
 while IFS= read -r -d '' alethe_file; do
@@ -52,7 +76,7 @@ while IFS= read -r -d '' alethe_file; do
     # Check if the paired .smt2 file exists
     if [ -f "$smt2_file" ]; then
         # Process the files and store the result
-        output=$(process_file "$alethe_file" "$smt2_file")
+        output=$(process_file "$alethe_file" "$smt2_file" "$sharing")
         return_value=$?
         echo "$output"
 
